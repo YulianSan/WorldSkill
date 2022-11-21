2ad8a0a323737d62a6bc60ee94f4dda312d05c26
YulianSan <knoob0733@gmail.com>
2022-11-20 21:32:36 -0300

    tentando resolver a task 22 de shanguai

--- Front-end/shanghai/task-22-js/script.js
+++ Front-end/shanghai/task-22-js/script.js
@@ -1,11 +1,22 @@
 const columns = document.querySelectorAll(".column");
 
 document.addEventListener("dragstart", (e) => {
+  
+  var div_base = document.createElement('div');
+  div_base.classList.add('img_drag');
+  var crt = e.target.cloneNode(true);
+  div_base.appendChild(crt);
+  document.body.appendChild(div_base);
+  e.dataTransfer.setDragImage(div_base, 140, 100);
+
   e.target.classList.add("dragging");
 });
 
 document.addEventListener("dragend", (e) => {
   e.target.classList.remove("dragging");
+  const img_drag = document.getElementsByClassName('img_drag')[0];
+
+  document.body.removeChild(img_drag);
 });
 
 columns.forEach((item) => {
