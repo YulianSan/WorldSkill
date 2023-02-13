bdf0e6d51cd92a426af163a1dfedd580f97b6d80
YulianSan <knoob0733@gmail.com>
2023-02-13 16:31:35 -0300

    Tudo que fiz

--- Front-end/shanghai/task-13-js/index.js
+++ Front-end/shanghai/task-13-js/index.js
@@ -5,20 +5,21 @@ inp_text.addEventListener('keyup', showOut);
 
 function showOut(){
     if(out_text.textContent === inp_text.value) return;
+    console.log('oi')
     textMarkdown = inp_text.value;
     let regexpBold = /\*\*(( {0,}\S{1,} {0,}){1,})\*\*/gi;
     let regexpTitle = /#{1}(( {0,}\S{1,} {0,}){1,})/gi;
     let regexpList = /-{1}(( {0,}\S{1,} {0,}){1,})/gi;
     let regexpLink =/[^!]\[(( {0,}\S{1,} {0,}){1,})\]\((( {0,}\S{1,} {0,}){1,})\)/gi;
-    let regexpImg =/[!]{1}\[(( {0,}\S{1,} {0,}){1,})\]\((( {0,}\S{1,} {0,}){1,})\)/gi;
+    let regexpImg =/[!]{1}\[(\S*)\]\((\S*)\)/gi;
     let regexpImgUrl =/[!]{1}\[(( {0,}\S{1,} {0,}){1,})\]\((( {0,}\S{1,} {0,}){1,})\)/gi;
 
-    textMarkdown = textMarkdown.replaceAll(/---/gi, '<hr>')
-    textMarkdown = textMarkdown.replaceAll(regexpBold, '<b>$1</b>');
-    textMarkdown = textMarkdown.replaceAll(regexpTitle, "<h1>$1</h1>");
-    textMarkdown = textMarkdown.replaceAll(regexpList, "<li>$1</li>");
-    textMarkdown = textMarkdown.replaceAll(regexpLink, "<a href='$3'>$1</a>");
-    textMarkdown = textMarkdown.replaceAll(regexpImg, "<img src='$3' alt='$1'/>$1");
+    textMarkdown = textMarkdown.replaceAll(regexpImg, "<img src='$2' alt='$1'>");
+    // textMarkdown = textMarkdown.replaceAll(/---/gi, '<hr>')
+    // textMarkdown = textMarkdown.replaceAll(regexpBold, '<b>$1</b>');
+    // textMarkdown = textMarkdown.replaceAll(regexpTitle, "<h1>$1</h1>");
+    // textMarkdown = textMarkdown.replaceAll(regexpList, "<li>$1</li>");
+    // textMarkdown = textMarkdown.replaceAll(regexpLink, "<a href='$3'>$1</a>");
     
     out_text.innerHTML = textMarkdown;
     
